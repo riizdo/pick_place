203698e782acbe7da7e35f4fbaa5d54485d8ae38
riizdo <ricardoizquierdopro@gmail.com>
2020-05-11 23:10:42 +0200

    first commit

---
+++ pick_place.ino
@@ -0,0 +1,19 @@
+/*  file: pick_place.ino
+ *  author: riizdo
+ *  date: 11/05/2020
+ *  version: 0.0
+ *  description: main for pick & place
+ */
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(115200);
+
+}
+
+void loop() {
+
+
+}
